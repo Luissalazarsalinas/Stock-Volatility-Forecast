95f099488abe3def7f62b6013398f3818d73b83c
Luis Fernando Salazar Salinas <87732612+Luissalazarsalinas@users.noreply.github.com>
2022-12-06 08:14:25 -0500

    Delete stock_project directory

--- stock_project/Lib/site-packages/arch/compat/pandas.py
+++
@@ -1,6 +0,0 @@
-try:
-    from pandas.api.types import is_datetime64_any_dtype
-except ImportError:
-    from pandas.core.common import is_datetime64_any_dtype
-
-__all__ = ["is_datetime64_any_dtype"]
