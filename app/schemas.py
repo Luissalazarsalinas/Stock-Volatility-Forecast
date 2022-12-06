ff2845b8da98d2af8eae9dd5e49d70f352d8cb28
Luissalazarsalinas <lf.salazarsalinas@gmail.com>
2022-12-06 09:31:42 -0500

    modified schemas and requirements

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# # fit in 
-# class FitIn(BaseModel):
-#     ticker:str
-#     use_new_data:bool
-#     n_observations:int
-#     p:int
-#     q:int
-# # fit out 
-# class FitOut(FitIn):
-#     success:bool
-    # message:str
 # Predict in
 class PredictIn(BaseModel):
     ticker:str
